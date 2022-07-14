cf25f09a8b6a18ae664eb91b9b12a2658cace710
AgustinMarceloRemeta <agustinmarceloremeta@gmail.com>
2022-07-13 21:03:29 -0300

    First Test

--- Assets/Develoment/BasicSpawner.cs
+++ Assets/Develoment/BasicSpawner.cs
@@ -5,16 +5,45 @@ using System;
 using Fusion;
 using Fusion.Sockets;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class BasicSpawner : MonoBehaviour, INetworkRunnerCallbacks
 {
     private NetworkRunner _runner;
-    public Button HostButton, StartButton;
+    [SerializeField] private NetworkPrefabRef _playerPrefab;
+    private Dictionary<PlayerRef, NetworkObject> _spawnedCharacters = new Dictionary<PlayerRef, NetworkObject>();
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    { // Create a unique position for the player
+        Vector3 spawnPosition = new Vector3((player.RawEncoded % runner.Config.Simulation.DefaultPlayers) * 3, 1, 0);
+        NetworkObject networkPlayerObject = runner.Spawn(_playerPrefab, spawnPosition, Quaternion.identity, player);
+        // Keep track of the player avatars so we can remove it when they disconnect
+        _spawnedCharacters.Add(player, networkPlayerObject);
+    }
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {  // Find and remove the players avatar
+        if (_spawnedCharacters.TryGetValue(player, out NetworkObject networkObject))
+        {
+            runner.Despawn(networkObject);
+            _spawnedCharacters.Remove(player);
+        }
+    }
+    public void OnInput(NetworkRunner runner, NetworkInput input) {
+        var data = new NetworkInputData();
+        // CAMBIAR AL MOVIMIENTO DEL AUTO
 
-    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player) { }
-    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
-    public void OnInput(NetworkRunner runner, NetworkInput input) { }
+        if (Input.GetKey(KeyCode.W))
+            data.direction += Vector3.forward;
+
+        if (Input.GetKey(KeyCode.S))
+            data.direction += Vector3.back;
+
+        if (Input.GetKey(KeyCode.A))
+            data.direction += Vector3.left;
+
+        if (Input.GetKey(KeyCode.D))
+            data.direction += Vector3.right;
+
+        input.Set(data);
+    }
     public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
     public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
     public void OnConnectedToServer(NetworkRunner runner) { }

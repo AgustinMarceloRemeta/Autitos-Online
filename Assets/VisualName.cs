9024937da8b25c091417f4be7be1f8bcbda53763
AgustinMarceloRemeta <agustinmarceloremeta@gmail.com>
2022-08-06 19:48:36 -0300

    aa

--- Assets/VisualName.cs
+++ Assets/VisualName.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VisualName : MonoBehaviour
 {
     GameObject cam;
+    [SerializeField] float distance;
     void Start()
     {
         cam = GameObject.Find("FollowText");
@@ -13,6 +15,8 @@ public class VisualName : MonoBehaviour
 
     void Update()
     {
+        if (Vector3.Distance(this.transform.position, cam.transform.position) > distance) this.GetComponent<Text>().enabled = false;
+        else this.GetComponent<Text>().enabled = false;
         this.transform.LookAt(cam.transform);
     }
 }
